175219ec5ee950d9f941468967773aba47293a6d
Daniel Thomas <80318628+tr3yd4n@users.noreply.github.com>
2025-04-12 17:40:33 +0100

    Added sections
    Added the 3 required sections for the design - will now begin to add content in section by section top to bottom

--- gfd-spa/src/components/HelloWorld.vue
+++ gfd-spa/src/components/HelloWorld.vue
@@ -1,11 +1,33 @@
 <script setup>
 </script>
 
+
 <template>
- <h2 class="text-blue-900 text-5xl">GFD SPA
-</h2>
-<p>test</p>
+
+  <!-- Our Story -->
+  <section class="w-full py-16 bg-gray-100">
+    <h2 class="text-3xl text-center mb-8">Our Story</h2>
+    <div class="container mx-auto px-4">
+    </div>
+  </section>
+
+  <!-- Company Values -->
+  <section class="w-full py-16 bg-gray-50">
+    <h2 class="text-3xl text-center mb-8">Company Values</h2>
+    <div class="container mx-auto px-4">
+    </div>
+  </section>
+
+  <!-- The Team -->
+  <section class="w-full py-16 bg-gray-100">
+    <h2 class="text-3xl text-center mb-8">The Team</h2>
+    <div class="container mx-auto px-4">
+    </div>
+  </section>
+  
 </template>
+
+
 
 <style scoped>
 </style>
