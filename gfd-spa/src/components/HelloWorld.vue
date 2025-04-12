00cd3fdf24bede4b6e558c2e40a6626270e43dca
Daniel Thomas <80318628+tr3yd4n@users.noreply.github.com>
2025-04-12 18:02:38 +0100

    Our story section elements
    I have started adding in the elements for the our story section and breaking them into what I think will be the best way to approach the responsiveness required by the design. I will change images and add fixes to correct styling of text elements later

--- gfd-spa/src/components/HelloWorld.vue
+++ gfd-spa/src/components/HelloWorld.vue
@@ -8,8 +8,20 @@
   <section class="w-full py-16 bg-gray-100">
     <h2 class="text-3xl text-center mb-8">Our Story</h2>
     <div class="container mx-auto px-4">
+      <div>
+        <img src="https://picsum.photos/536/354" class="w-[280px] h-[155px] object-cover border-[7px] border-white" />
+        <div class="flex items-center mt-4">
+          <p class="italic font-normal text-[14.4px] mr-2">Bi-Fold Doors</p>
+          <div class="w-6 h-6 bg-black rounded-full"></div>
+          <div class="w-[60px] h-[1px] bg-black ml-2"></div>
+        </div>
+      </div>
+      <div>
+        <img src="https://picsum.photos/536/354" class="w-[684px] h-[473px] object-cover ml-[22px]" />
+      </div>
     </div>
   </section>
+
 
   <!-- Company Values -->
   <section class="w-full py-16 bg-gray-50">
@@ -24,7 +36,6 @@
     <div class="container mx-auto px-4">
     </div>
   </section>
-  
 </template>
 
 
