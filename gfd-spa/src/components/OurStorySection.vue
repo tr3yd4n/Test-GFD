963d3d4248df6431103eeeea24630a0450f9526a
Daniel Thomas <80318628+tr3yd4n@users.noreply.github.com>
2025-04-15 08:25:45 +0100

    Our Story Section
    Main Product Group has been added in on mobile with images and placeholders - some minor sizing and transformational changes needed
    containers added for lg images plugging in

--- gfd-spa/src/components/OurStorySection.vue
+++ gfd-spa/src/components/OurStorySection.vue
@@ -21,7 +21,7 @@
         >
           Our Story
         </h2>
-        <hr class="w-full border-t border-black my-4" />
+        <hr class="w-full border-t border-black my-2" />
         <p
           class="text-sm md:text-base lg:text-md pr-6 max-w-[468px]"
           style="font-family: 'Red Hat Display', sans-serif"
@@ -40,7 +40,54 @@
           Our goal was always to offer a complete home improvement solution,
           which we now carry out nationwide through GFD Homes.
         </p>
+        Styling Options
+<div class="w-[701px] h-96 relative">
+  <div class="w-40 h-64 p-1.5 left-[395.28px] top-[52.82px] absolute origin-top-left rotate-[11.77deg] bg-white shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] inline-flex justify-start items-center gap-2.5">
+    <img class="w-44 self-stretch" src="https://placehold.co/169x261" />
+  </div>
+  <div class="w-64 h-52 left-[207.64px] top-[387.92px] absolute origin-top-left rotate-[-105deg] bg-white shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"></div>
+  <img class="w-64 h-52 left-[218.82px] top-[393.33px] absolute origin-top-left rotate-[-105deg]" src="https://placehold.co/270x202" />
+  <div class="w-44 h-72 left-[232.72px] top-[27.06px] absolute bg-white shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"></div>
+  <img class="w-40 h-64 left-[240.39px] top-[33.68px] absolute" src="https://placehold.co/157x256" />
+  <div class="w-36 h-60 p-[5px] left-[443.07px] top-[109.86px] absolute origin-top-left rotate-[11.77deg] bg-white shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] inline-flex justify-start items-center gap-2.5">
+    <img class="w-36 self-stretch" src="https://placehold.co/150x234" />
+  </div>
+  <div class="w-24 h-0 left-[189px] top-[210.12px] absolute origin-top-left -rotate-180 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] outline outline-1 outline-offset-[-0.50px] outline-black"></div>
+  <div class="w-20 h-8 left-0 top-[194.12px] absolute justify-start text-black text-xl font-normal font-['Red_Hat_Display']">Decking </div>
+  <div class="w-16 h-8 left-[634px] top-[205px] absolute justify-start text-white text-xl font-normal font-['Red_Hat_Display']">iBlinds</div>
+  <div class="left-[75px] top-[27.79px] absolute origin-top-left rotate-[-5.38deg] justify-start text-black text-lg font-normal font-['Red_Hat_Display']">Roof Light</div>
+  <div class="w-24 h-0 left-[292px] top-[69.12px] absolute origin-top-left -rotate-180 outline outline-1 outline-offset-[-0.50px] outline-black"></div>
+  <div class="w-8 h-0 left-[204px] top-[69.12px] absolute origin-top-left -rotate-90 outline outline-1 outline-offset-[-0.50px] outline-black"></div>
+  <div class="w-6 h-0 left-[179px] top-[35.12px] absolute outline outline-1 outline-offset-[-0.50px] outline-black"></div>
+  <div class="w-14 h-0 left-[624px] top-[221.12px] absolute outline outline-1 outline-offset-[-0.50px] outline-white"></div>
+
+
+</div>
       </div>
+      <!--group-->
+      <!-- <div class="absolute top-[537px] left-[-74px]">
+        <div class="w-96 h-96 relative">
+          <img
+            class="w-28 h-52 left-[216.31px] top-[52.49px] absolute origin-top-left rotate-[11.77deg]"
+            src="https://placehold.co/116x201"
+          />
+
+          <img
+            class="w-60 h-44 left-[87.16px] top-[353.53px] absolute origin-top-left rotate-[-105deg]"
+            src="https://placehold.co/232x174"
+          />
+
+          <img
+            class="w-32 h-56 left-[67.53px] top-[20.16px] absolute"
+            src="https://placehold.co/135x220"
+          />
+
+          <img
+            class="w-28 h-48 left-[236.05px] top-[90.49px] absolute origin-top-left rotate-[11.77deg]"
+            src="https://placehold.co/111x193"
+          />
+        </div>
+      </div> -->
 
       <div
         class="absolute transition-all duration-700 ease-in-out w-72 h-40 top-[13px] left-[125px] shadow md:w-[455px] md:h-64 md:top-[78px] md:left-[986px] bg-white"
@@ -63,17 +110,47 @@
         Bi-Fold <br />Doors
       </div>
       <img
-        class="absolute top-[24px] left-[-300px] w-[463px] h-[463px] shadow-sm hidden lg:block"
+        class="absolute top-[24px] left-[-300px] w-[463px] h-[463px] shadow-sm hidden md:block"
         src="/images/ourStoryImages/Rectangle 414.jpg"
       />
 
       <img
-        class="w-[103px] h-[244px] left-[90px] border-4 border-white top-[30px] absolute"
+        class="w-[103px] h-[244px] left-[90px] border-4 border-white top-[30px] absolute hidden md:block"
         src="/images/ourStoryImages/blueDoor.jpg"
       />
     </div>
   </section>
   <section class="w-full mt-32 lg:hidden relative mb-60">
+    <!--Group-->
+    <div class="absolute top-[-377px] left-[-74px] z-50">
+  <div class="w-96 h-96 relative">
+        <!-- Shadows behind Antique Oak Seat image -->
+    <div class="w-56 h-44 left-[59.87px] top-[339.19px] absolute origin-top-left rotate-[-105deg] bg-white shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"></div>
+    <div class="w-60 h-48 left-[65px] top-[348.65px] absolute origin-top-left rotate-[-105deg] bg-white shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"></div>
+    <img class="w-60 h-44 left-[57.16px] top-[323.53px] absolute origin-top-left rotate-[-82deg]" src="/images/ourStoryImages/Antique Oak Seatch 3.png" />
+    <!-- Shadow behind first metallic silver image -->
+    <div class="w-28 h-52 left-[240.01px] top-[41.53px] absolute origin-top-left rotate-[25deg] bg-white shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"></div>
+    <img class="w-28 h-52 left-[216.31px] top-[52.49px] absolute origin-top-left rotate-[11.77deg]" src="/images/ourStoryImages/metallic-silver-closed 3.png" />
+
+
+
+    <!-- Shadow behind background rectangle -->
+    <div class="w-36 h-60 left-[61.02px] top-[12px] absolute bg-white shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"></div>
+    <img class="w-32 h-56 left-[67.53px] top-[20.16px] absolute" src="/images/ourStoryImages/Rectangle 465.png" />
+
+    <!-- Shadow behind second metallic silver image -->
+    <div class="w-28 h-52 p-[5px] left-[258.07px] top-[79.81px] absolute origin-top-left rotate-[22.75deg] bg-white shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"></div>
+    <img class="w-28 h-48 left-[236.05px] top-[90.49px] absolute origin-top-left rotate-[11.77deg]" src="/images/ourStoryImages/metallic-silver-closed 4.png" />
+
+    <div
+        class="absolute text-black font-['Red_Hat_Display'] font-normal text-sm top-[78px] left-[47px] md:text-base md:top-[372px] md:left-[1018px] transition-all duration-700 ease-in-out"
+      >
+        Decking
+      </div>
+
+  </div>
+</div>
+
     <p
       class="font-redhat sm:w-full h-[286px] text-sm md:text-base lg:text-lg pt-4 ml-12 pl-6 bg-neutral-100"
     >
